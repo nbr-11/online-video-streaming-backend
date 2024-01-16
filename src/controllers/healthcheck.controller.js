0721a4c38539da7fc0a1ebfeb4d03c05bab3f6c4
Nishant Rai <nbr.11.work@gmail.com>
2024-01-16 21:09:37 +0530

    fixed the import statements in healthcheck.controller.js and healthcheck.router.js

--- src/controllers/healthcheck.controller.js
+++ src/controllers/healthcheck.controller.js
@@ -1,6 +1,6 @@
-import { ApiError } from "../utils/ApiError";
-import { ApiResponse } from "../utils/ApiResponse";
-import { asyncHandler } from "../utils/asyncHandler";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
 
 const healthCheck = asyncHandler(async(req,res) => {
     return res 
